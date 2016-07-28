debd62cb680fcc682c9ebbabb11b6f0b32e9d5ca
Christopher Khoo <khooweiwen@gmail.com>
2016-07-29 07:49:17 +0800

    Add account activations

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   delete  '/logout',  to: 'sessions#destroy'
   # patch   '/edit_user', to: 'user#update'
   resources :users
-
+  resources :account_activations, only: [:edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
